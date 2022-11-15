fa6d6967abaa24cd4cc676627bf5dc601cce6d0c
jm server2 <jm@mazoea.com>
2022-11-15 20:41:55 +0100

    updated script

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,11 @@
-git push origin :v8 :latest
-git tag -d v8 latest
-git tag v8 latest
+git push origin :v8
+git tag -d v8
+git tag v8
+
+git push origin :latest
+git tag -d latest
+git tag latest
+
 git push origin master --tags 
 
 IF "%1"=="nopause" GOTO No1
